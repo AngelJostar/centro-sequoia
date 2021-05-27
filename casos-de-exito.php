1921891d70dd5072c65467b95ef1affd3a167aa0
AngelJostar <angelrojas@ciencias.unam.mx>
2021-05-27 01:16:46 -0500

    commit all

--- casos-de-exito.php
+++ casos-de-exito.php
@@ -7,7 +7,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="format-detection" content="telephone=no">
-<title>Crecimiento infantil por edad</title>
+<title>Casos de éxito</title>
 <link rel="stylesheet" rel="preload" href="/css/style.css" media="all">
 <link rel="stylesheet" rel="preload" href="/css/style-slider.css" media="all">
 <link rel="stylesheet" rel="preload" href="/css/normalize.css" media="all">>
