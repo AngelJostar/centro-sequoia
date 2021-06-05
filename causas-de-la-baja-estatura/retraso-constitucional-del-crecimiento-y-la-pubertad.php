e403aea6676db7942255afbc19cd3931d3c61fca
AngelJostar <angelrojas@ciencias.unam.mx>
2021-06-05 02:01:07 -0500

    commit all

--- causas-de-la-baja-estatura/retraso-constitucional-del-crecimiento-y-la-pubertad.php
+++ causas-de-la-baja-estatura/retraso-constitucional-del-crecimiento-y-la-pubertad.php
@@ -7,7 +7,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="format-detection" content="telephone=no">
-<meta name="description" content="El retraso constitucional del crecimiento y de la pubertad (RCCP) es una condición caracterizada por baja estatura y maduración esquelética retrasada, en niños sanos.">
+<meta name="description" content="El retraso constitucional del crecimiento y la pubertad (RCCP) es una condición caracterizada por baja estatura y maduración esquelética retrasada, en niños sanos.">
 <title>Pubertad atrasada - Especialistas endocrinólogos pediatras</title>
 <link rel="stylesheet" rel="preload" href="/css/style.css" media="all">
 <link rel="stylesheet" rel="preload" href="/css/normalize.css" media="all">
