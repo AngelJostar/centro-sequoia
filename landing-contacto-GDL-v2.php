a9637676aae705e90dd214258cd4f0fe00a4dd66
AngelJostar <angelrojas@ciencias.unam.mx>
2021-07-13 19:14:56 -0500

    commit all

--- landing-contacto-GDL-v2.php
+++ landing-contacto-GDL-v2.php
@@ -54,10 +54,10 @@
                 <div>
                     <div class="tercer-p">
                         <h2 class="font-ciudades">Guadalajara<br>
-                                <a href="tel:333615-2825">
-                                    <span>Tel. (33) 36152825</span>
+                                <a href="tel:3336152825">
+                                    <span>Tel. (33) 3615-2825</span>
                                 </a> <br>
-                                <a href="https://api.whatsapp.com/send?phone=523333371392&text=%EF%BF%BD%20Hola!%20Quiero%20informaci%C3%B3n%20de%20Centro%20Sequoia.%0A" target="_blank"><span>WA: (33) 3337-1392</span></a>
+                                <a href="https://api.whatsapp.com/send?phone=523333371382&text=%EF%BF%BD%20Hola!%20Quiero%20informaci%C3%B3n%20de%20Centro%20Sequoia.%0A" target="_blank"><span>WA: (33) 3337-1382</span></a>
                         </h2>
                         <p><span class="font-bold">DIRECCIÓN:</span> José María Vigil #2750, Colonia Providencia,</p>
                         <p>C.P.44639, Guadalajara, Jalisco</p>
